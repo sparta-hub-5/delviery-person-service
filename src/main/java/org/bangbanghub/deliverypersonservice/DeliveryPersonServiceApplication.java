6b74fec07f7f3fe1d6c5fb752917bc0c8dfe9fe4
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-11-25 18:29:25 +0900

    first commit

---
+++ src/main/java/org/bangbanghub/deliverypersonservice/DeliveryPersonServiceApplication.java
@@ -0,0 +1,13 @@
+package org.bangbanghub.deliverypersonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPersonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryPersonServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bangbanghub/deliverypersonservice/DeliveryPersonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bangbanghub.deliverypersonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPersonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
